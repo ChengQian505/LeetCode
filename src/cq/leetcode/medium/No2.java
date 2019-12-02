da6456f463921b0e9c1aa215384186b40cce7e07
程前 <18239891201@sina.cn>
2019-12-02 15:17:23 +0800

    add No2,No7,No9

---
+++ src/cq/leetcode/medium/No2.java
@@ -0,0 +1,69 @@
+package cq.leetcode.medium;
+
+import cq.leetcode.util.ListNode;
+
+
+/**
+ * 2.两数相加
+ */
+public class No2 {
+
+    public static void main(String[] args) {
+        System.out.println(addTwoNumbers(
+                        ListNode.getListNode(new Integer[]{2, 4, 3}),
+                        ListNode.getListNode(new Integer[]{5, 6, 4})
+                ));
+        System.out.println(addTwoNumbers(
+                        ListNode.getListNode(new Integer[]{5}),
+                        ListNode.getListNode(new Integer[]{5})
+                ));
+        System.out.println(addTwoNumbers(
+                        ListNode.getListNode(new Integer[]{1,8}),
+                        ListNode.getListNode(new Integer[]{0})
+                ));
+    }
+
+    private static boolean needAdd=false;
+
+    private static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode listNode = null;
+        while (l1 != null || l2 != null) {
+            int num = 0;
+            if (l1 != null) {
+                num += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                num += l2.val;
+                l2 = l2.next;
+            }
+            if (needAdd){
+                num++;
+            }
+            ListNode listNode1;
+            if (num > 9) {
+                listNode1 = new ListNode(num % 10);
+                if (l1==null&&l2==null){
+                    listNode1.next=new ListNode(1);
+                    needAdd=false;
+                }else{
+                    needAdd=true;
+                }
+            } else {
+                listNode1 = new ListNode(num);
+                needAdd=false;
+            }
+            if (listNode!=null){
+                ListNode listNode2=listNode;
+                while (listNode2.next!=null){
+                    listNode2=listNode2.next;
+                }
+                listNode2.next = listNode1;
+            }else{
+                listNode = listNode1;
+            }
+        }
+        return listNode;
+    }
+
+}
--- src/cq/leetcode/util/ListNode.java
+++ src/cq/leetcode/util/ListNode.java
@@ -13,4 +13,26 @@ public class ListNode {
             return val+","+next.toString();
         }
     }
+
+    /**
+     * 生成ListNode
+     * @param args
+     * @return
+     */
+    public static ListNode getListNode(Integer[] args){
+        if (args.length==0){
+            return null;
+        }
+        ListNode listNode1 = null;
+        for (int i=args.length-1;i>=0;i--){
+            if (listNode1==null){
+                listNode1=new ListNode(args[i]);
+            }else{
+                ListNode listNode2=new ListNode(args[i]);
+                listNode2.next=listNode1;
+                listNode1=listNode2;
+            }
+        }
+        return listNode1;
+    }
 }
